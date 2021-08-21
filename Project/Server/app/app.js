afe5a54da2222e6e4eae647d5a6e1ecacd7ecfc5
ThanhB1805916 <thanhb1805916@student.ctu.edu.vn>
2021-08-21 09:32:25 +0700

    Update server

--- Project/Server/app/app.js
+++ Project/Server/app/app.js
@@ -15,18 +15,3 @@ router(app);
 app.listen(port, () => {
   console.log("Server is listening on " + port);
 });
-
-
-const app = express();
-const port = process.env.PORT || 8080;
-
-//Thêm middleware cái này dùng xử lý body(JSON) của request
-//app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.json());
-
-// routes(app);
-require("./routes")(app);
-app.listen(port, ()=>{
-  console.log("Server is listening on " + port);  
-})
-
