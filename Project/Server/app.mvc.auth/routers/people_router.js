9c73aba7764532daba8912e262d5ebf6fcda0ab6
ThanhB1805916 <thanhb1805916@student.ctu.edu.vn>
2021-08-21 09:28:27 +0700

    Update server

--- Project/Server/app.mvc.auth/routers/people_router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const PeopleController = require("../controllers/PeopleController");
-let controller = new PeopleController();
-
-router.route("/").get(controller.getList);
-
-module.exports = router;
