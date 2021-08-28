37c7517d3cb72bdabc607158b58cefb010652311
ThanhB1805916 <thanhb1805916@student.ctu.edu.vn>
2021-08-28 08:02:01 +0700

    hoàn thành kế hoạch phần mềm

--- Project/Server/app/controllers/ProductsController.js
+++ Project/Server/app/controllers/ProductsController.js
@@ -12,8 +12,22 @@ module.exports = class ProductsController {
   getProductByNo = async (req, res) => {
     const { pro_no } = req.params;
 
+    if (pro_no < 0) {
+      return res.status(400).json();
+    }
+
     const product = await this.dao.getProductByNo(pro_no);
 
+    const notExistProduct = !this.existProduct(product);
+
+    if (notExistProduct) {
+      return res.status(404).json({});
+    }
+
     return res.json(product);
+  };
+
+  existProduct = (product) => {
+    return product !== undefined && product !== {};
   };
 };
