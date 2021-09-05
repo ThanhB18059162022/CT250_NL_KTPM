9bbcaa0bb4aef804eafb1020e675365afef6f263
ThanhB1805916 <thanhb1805916@student.ctu.edu.vn>
2021-09-05 18:24:30 +0700

    Update Auth model validator

--- Project/Server/app/controllers/authentication_controllers/AuthenticationValidator.js
+++ Project/Server/app/controllers/authentication_controllers/AuthenticationValidator.js
@@ -6,10 +6,10 @@ const { getValidationResult } = require("../validatorHelper");
 module.exports = class AuthenticationValidator {
   getResult = (schema, data) => getValidationResult(schema, data);
 
-  validateLoginModel = (loginModel) => {
+  validateLoginModel = (loginModel = {}) => {
     const schema = joi.object({
-      username: joi.string().min(5).max().required(),
-      password: joi.string().min(5).max().required(),
+      username: joi.string().min(5).max(70).alphanum().required(),
+      password: joi.string().min(5).max(64).required(),
     });
 
     const result = this.getResult(schema, loginModel);
