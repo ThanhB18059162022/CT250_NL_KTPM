a9877e37d919ad89f850a425b102646700a7081a
ThanhB1805916 <thanhb1805916@student.ctu.edu.vn>
2021-09-05 18:24:30 +0700

    Update Auth model validator

--- Project/Server/test/controllers_tests/authentication_controllers_tests/AuthenticationValidator.test.js
+++ Project/Server/test/controllers_tests/authentication_controllers_tests/AuthenticationValidator.test.js
@@ -1,5 +1,4 @@
 const AuthenticationValidator = require("../../../app/controllers/authentication_controllers/AuthenticationValidator");
-
 // Test các hàm xác thực dữ liệu của lớp AuthenticationValidator
 
 function getValidator() {
@@ -7,11 +6,236 @@ function getValidator() {
 }
 
 describe("Xác thực model đăng nhập", () => {
+  test("Model hợp lệ", () => {
+    //Arrange
+    const loginModel = {
+      username: "valid",
+      password: "valid",
+    };
+
+    const failed = false;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Model undefined", () => {
+    //Arrange
+    const loginModel = undefined;
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Model null", () => {
+    //Arrange
+    const loginModel = null;
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Model empty", () => {
+    //Arrange
+    const loginModel = {};
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+});
+
+describe("Xác thực model đăng nhập - tài khoản", () => {
   test("Tài khoản undefined", () => {
     //Arrange
     const loginModel = {
       username: undefined,
       password: "valid",
+    };
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Tài khoản bỏ trống", () => {
+    //Arrange
+    const loginModel = {
+      username: "  ",
+      password: "valid",
+    };
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Tài khoản ngắn hơn 5", () => {
+    //Arrange
+    const loginModel = {
+      username: "tk",
+      password: "valid",
+    };
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Tài khoản dài hơn 70", () => {
+    //Arrange
+    const loginModel = {
+      username:
+        "aaaaaaaaaaa8fad5592ed3d048090aa7d80fc2a4c4207fe936aeda98af4293956375465",
+      password: "valid",
+    };
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Tài khoản alphanum - không có khoảng trắng", () => {
+    //Arrange
+    const loginModel = {
+      username: "tk tk1",
+      password: "valid",
+    };
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+});
+
+describe("Xác thực model đăng nhập - mật khẩu", () => {
+  test("Mật khẩu undefined", () => {
+    //Arrange
+    const loginModel = {
+      password: undefined,
+      username: "valid",
+    };
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Mật khẩu bỏ trống", () => {
+    //Arrange
+    const loginModel = {
+      password: "  ",
+      username: "valid",
+    };
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Mật khẩu ngắn hơn 5", () => {
+    //Arrange
+    const loginModel = {
+      password: "tk",
+      username: "valid",
+    };
+
+    const failed = true;
+
+    const validator = getValidator();
+
+    //Act
+    const expRes = failed;
+    const actRes = validator.validateLoginModel(loginModel).hasAnyError;
+
+    //Expect
+    expect(actRes).toEqual(expRes);
+  });
+
+  test("Mật khẩu dài hơn 64", () => {
+    //Arrange
+    const loginModel = {
+      password:
+        "aaaaa8fad5592ed3d048090aa7d80fc2a4c4207fe936aeda98af4293956375465",
+      username: "valid",
     };
 
     const failed = true;
