ae4e9b43f0883c70eae901f38e8cf4c0417bb89a
ThanhB1805916 <thanhb1805916@student.ctu.edu.vn>
2021-09-05 17:56:06 +0700

    Create AuthenticationValidator.test.js

---
+++ Project/Server/test/controllers_tests/authentication_controllers_tests/AuthenticationValidator.test.js
@@ -0,0 +1,11 @@
+const AuthenticationValidator = require("../../../app/controllers/authentication_controllers/AuthenticationValidator");
+
+// Test các hàm xác thực dữ liệu của lớp AuthenticationValidator
+
+describe("Xác thực tài khoản", () => {
+  test("Tài khoản undefined", () => {
+    //Arrange
+    //Act
+    //Expect
+  });
+});
