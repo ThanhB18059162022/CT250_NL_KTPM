aa69d4699a15f4bcdebfbd486c8559d6c6c33af0
ThanhB1805916 <thanhb1805916@student.ctu.edu.vn>
2021-09-09 08:12:02 +0700

    Thêm lớp xác nhận dữ liệu của quản trị viên
    Xác nhận mã,
    xác nhận số điện thoại

---
+++ Project/Server/app/controllers/moderators_controllers/ModeratorsValidator.js
@@ -0,0 +1,33 @@
+// Tham khảo https://joi.dev/api/?v=17.4.2
+//https://stackoverflow.com/questions/57993305/how-can-i-validate-number-of-digits-from-joi-using-nodejs
+
+const joi = require("joi");
+const { getValidationResult } = require("../validatorHelper");
+
+module.exports = class ModeratorsValidator {
+  getResult = (schema, data) => getValidationResult(schema, data);
+
+  validateNo = (mod_no) => {
+    const schema = joi.object({
+      mod_no: joi.number().integer().min(0).required(),
+    });
+
+    const result = this.getResult(schema, { mod_no });
+
+    return result;
+  };
+
+  validatePhoneNumber = (mod_phoneNumber) => {
+    const schema = joi.object({
+      mod_phoneNumber: joi
+        .string()
+        .length(10)
+        .pattern(/^[0-9]+$/)
+        .required(),
+    });
+
+    const result = this.getResult(schema, { mod_phoneNumber });
+
+    return result;
+  };
+};
