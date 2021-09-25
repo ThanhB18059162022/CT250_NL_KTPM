5d30c2db2c5d37b51d0c32e4445abf5806b834f0
ThanhB1805916 <thanhb1805916@student.ctu.edu.vn>
2021-09-25 08:07:51 +0700

    Get latest feedback

--- Project/Server/app/controllers/feedback_controllers/FeedbackController.js
+++ Project/Server/app/controllers/feedback_controllers/FeedbackController.js
@@ -1,6 +1,48 @@
-module.exports = class FeedbackController {
+const Controller = require("../Controller");
+
+module.exports = class FeedbackController extends Controller {
+  constructor(validator, dao) {
+    super();
+    this.dao = dao;
+  }
+
+  //#region GET
+
+  // Lấy phản hồi từ ngày đó trở về trước
   getFeedback = async (req, res) => {
-    const {} = req;
-    return res.json();
+    const {
+      date = new Date(),
+      order = "DESC",
+      page = 1,
+      limit = 24,
+    } = req.query;
+
+    const { startIndex, endIndex } = this.getStartEndIndex(page, limit);
+
+    const feedback = await this.dao.getFeedback(
+      date,
+      order,
+      startIndex,
+      endIndex
+    );
+
+    const productsPage = this.getPaginatedResults(feedback, page, limit);
+
+    return res.json(productsPage);
   };
+
+  //#endregion
+
+  //#region ADD
+
+  addFeedback = async (req, res) => {
+    const { body: feedback } = req;
+
+    // const result = this.validator.validateFeedback(feedback);
+    // if(result.hasAnyError)
+
+    return res.status(400).json();
+  };
+
+  //#endregion
 };
--- Project/Server/test/controllers_tests/feedback_controllers_tests/FeedbackController.test.js
+++ Project/Server/test/controllers_tests/feedback_controllers_tests/FeedbackController.test.js
@@ -3,25 +3,71 @@ const { ResponseMock } = require("../controllerTestHelper");
 
 // Kiểm tra các api end-point của phản hồi
 
+//#region INIT
+
+const FEEDBACK = [
+  { fb: 1, date: new Date("2021-09-24") },
+  { fb: 2, date: new Date() },
+];
+
+class FeedbackDAOMock {
+  getFeedback = jest.fn(async () => FEEDBACK);
+}
+
+//#endregion
+
+let validatorMock;
+let daoMock;
 function getController() {
-  return new FeedbackController();
+  return new FeedbackController(validatorMock, daoMock);
 }
 
 describe("Lấy danh sách phản hồi", () => {
+  beforeEach(() => {
+    daoMock = new FeedbackDAOMock();
+  });
+
   test("Không query params", async () => {
     //Arrange
-    const feedback = [];
+    const feedback = FEEDBACK;
     const controller = getController();
 
-    const reqMock = {};
+    const reqMock = {
+      query: {},
+    };
     const resMock = new ResponseMock();
 
     //Act
-    const expRes = { statusCode: 200, items: { feedback } };
+    const expRes = { statusCode: 200, body: { items: feedback } };
     const actRes = await controller.getFeedback(reqMock, resMock);
 
     //Expect
     expect(resMock.json).toBeCalledTimes(1);
-    // expect(expRes).toEqual(actRes);
+    expect(expRes).toEqual(actRes);
+  });
+});
+
+describe("Thêm phản hồi", () => {
+  beforeEach(() => {
+    daoMock = new FeedbackDAOMock();
+  });
+
+  test("Phản hồi không hợp lệ - 400", async () => {
+    //Arrange
+    const feedback = undefined;
+    const controller = getController();
+
+    const reqMock = {
+      body: feedback,
+    };
+
+    const resMock = new ResponseMock();
+
+    //Act
+    const expRes = { statusCode: 400 };
+    const actRes = await controller.addFeedback(reqMock, resMock);
+
+    //Expect
+    expect(actRes).toEqual(expRes);
   });
 });
