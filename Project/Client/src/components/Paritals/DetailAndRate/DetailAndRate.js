4dc6c76f54a62e20752d38e0722e117cb4b0e102
firefly-gituser <65105006+firefly-gituser@users.noreply.github.com>
2021-10-08 19:16:38 +0700

    Update

--- Project/Client/src/components/Paritals/DetailAndRate/DetailAndRate.js
+++ Project/Client/src/components/Paritals/DetailAndRate/DetailAndRate.js
@@ -14,11 +14,11 @@ const DetailAndRate = ({ product, showDetail }) => {
                 </div>
 
                 <ul>
-                    {product.prod_feedbacks.map((item, index) =>
+                    {product.prod_feedbacks.length > 0 ? product.prod_feedbacks.map((item, index) =>
                         <li key={index}>
                             <Comment style={{ background: "#69baff33" }} title={item.fb_cusomter.cus_name} content={item.fb_content} options="left" time={Helper.Exchange.toLocalDate(item.fb_time)}
                                 children={item.fb_replies.map((e,idx) => <Comment key={idx} style={{ background: "#69baff33" }} title={e.rep_mod} content={e.rep_content} options="left" time={Helper.Exchange.toLocalDate(e.rep_time)} />)} />
-                        </li>)}
+                        </li>): <p>Chưa có đánh giá nào</p>}
                 </ul>
             </div>
             <div className="DetailInfo">
