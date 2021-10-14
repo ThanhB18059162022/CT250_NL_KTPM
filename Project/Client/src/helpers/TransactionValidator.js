fdb50458ea305ed3dad975cc2d07c8abd35f9a5f
firefly-gituser <65105006+firefly-gituser@users.noreply.github.com>
2021-10-14 15:50:11 +0700

    fix error

--- Project/Client/src/helpers/TransactionValidator.js
+++ Project/Client/src/helpers/TransactionValidator.js
@@ -16,7 +16,7 @@ const TransactionValidator = (()=>{
     }
 
     const checkingEmail = (email) =>{
-        if(isEmpty(email) || !email.match(/@/) || !email.match(/(?=^[^@])/) || !email.match(/[^@]+$/)){
+        if(isEmpty(email) || !email.match(/@/) || !email.match(/(?=^[^@])/) || !email.match(/[^@]+$/) || !email.includes('.')){
             return{
                 result:false,
                 resson:'Email không hợp lệ!'
