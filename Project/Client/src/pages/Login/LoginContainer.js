b4eb9f9b9e7c2f2d0a32fa7d2984f72bc386354c
firefly-gituser <65105006+firefly-gituser@users.noreply.github.com>
2021-10-14 15:54:39 +0700

    Add notify to login

--- Project/Client/src/pages/Login/LoginContainer.js
+++ Project/Client/src/pages/Login/LoginContainer.js
@@ -69,7 +69,11 @@ const LoginContainer = () => {
 
     let result = await auth.login(loginInfo);
 
-    result && history.push("/admin");
+    if(!result){
+      onError("Thông tin đăng nhập không đúng")
+      return
+    }
+    history.push("/admin");
   };
 
   return (
