d1f1aba1fb40c79badc442f74daf57a6f5a9a53e
ThanhB1805916 <59827241+ThanhB1805916@users.noreply.github.com>
2021-11-06 08:08:26 +0700

    Thêm màu vào payment (#92)
    * Update doc
    
    * Update prod color to payment

--- Project/Server/app/server.js
+++ Project/Server/app/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-const corsHandler = require("./routers/corsHandler");
 const router = require("./routers/router");
 const { errorHandler } = require("./routers/routerErrorHandler");
 const config = require("./config");
 const port = config.port;
-const cors = require('cors')
+const cors = require("cors");
+
 // Lọc các tên miền có quyền truy cập
-// corsHandler(app);
+app.use(cors());
 
 // Static file chứa hình
 app.use(express.static("public"));
-app.use(cors())
+
 //Thêm middleware cho Post với Put cái này dùng xử lý body(JSON) của request
 // Request Object as a JSON Object
 app.use(express.json({ limit: "5mb" })); // Kích thước tối đa 5mb
