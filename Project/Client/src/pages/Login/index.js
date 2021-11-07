67b891278f7acc13c6abddfe593503966db6999a
Firefly <tamthachchi@gmail.com>
2021-11-07 15:30:05 +0700

    Messsage

--- Project/Client/src/pages/Login/index.js
+++ Project/Client/src/pages/Login/index.js
@@ -1,15 +1,8 @@
 import Header from "../../components/Paritals/Header"
 import LoginContainer from "./LoginContainer"
 import "./Login.Style.scss"
-import { useEffect } from "react"
-// import { useHistory } from "react-router"
 const Login = () =>{
-    useEffect(()=>{
-        const cus =  document.querySelector('.fb_dialog_content')
-        if(!cus) return
-        cus.style.display ='none'
-        return () => cus.style.display='inherit'
-    })
+
     return(
         <div className="Login">
             <Header/>
